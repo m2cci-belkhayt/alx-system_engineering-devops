07562df7a2c795c63e8d8c7c43621637074ce601
Taha Belkhayate <69703213+m2cci-belkhayt@users.noreply.github.com>
2023-11-29 22:05:22 +0100

    task 5

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Check if an argument is provided
+if ARGV.empty?
+    puts "Usage: #{$PROGRAM_NAME} <input_string>"
+    exit 1
+  end
+  
+  # Get the input string from the command line argument
+  input_string = ARGV[0]
+  
+  # Define the regular expression
+  regex = /^h.n$/
+  
+  # Use the regular expression to find matches
+  matches = input_string.scan(regex)
+  
+  # Output the matches
+  puts matches.join
+  
